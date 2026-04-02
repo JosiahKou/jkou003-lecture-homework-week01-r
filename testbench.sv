b027e7ea201a9d136ea67fc269ee4b895b7f3b3e
JosiahKou <155879792+JosiahKou@users.noreply.github.com>
2026-04-02 22:06:44 +0000

    finished

--- testbench.sv
+++ testbench.sv
@@ -1,7 +1,5 @@
 `timescale 1ns / 1ps
 
-`include "halfadder.sv"
-
 module testbench();
 
 // Signals for UUT connection
@@ -13,6 +11,7 @@ reg pass;
 integer tests_failed;
 
 // Instantiate the unit under test
+halfadder uut(A, B, S, C);
 
 initial begin
     // Set up output to VCDD file
@@ -20,6 +19,8 @@ initial begin
     $dumpvars(0, testbench);
 
     // Initialize testbench variables
+    pass = 1;
+    tests_failed = 0;
 end
 
 // Write Test Stimulus
@@ -35,18 +36,30 @@ endtask
 task test_01();
     begin
         // Put your test for A = 0, and B = 1
+        A = 0;
+        B = 1;
+        #5;
+        pass &= S == 1 & C == 0;
     end
 endtask
 
 task test_10();
     begin
-        // Put your test for A = 0, and B = 1
+        // Put your test for A = 1, and B = 0
+        A = 1;
+        B = 0;
+        #5;
+        pass &= S == 1 & C == 0;
     end
 endtask
 
 task test_11 ();
     begin
-        // Put your test for A = 0, and B = 1
+        // Put your test for A = 1, and B = 1
+        A = 1;
+        B = 1;
+        #5;
+        pass &= S == 0 & C == 1;
     end
 endtask
 
